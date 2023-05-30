d19eeb64d364388b52a7216e4ab01068b44f64b5
Saif Islam Rayhan <134764307+rayhanosd@users.noreply.github.com>
2023-05-30 21:28:34 +0600

    sum

---
+++ saif1.c
@@ -0,0 +1,12 @@
+    #include<stdio.h>
+    int main ()
+    {
+        int a;
+        int b;
+        int sum;
+        a=60;
+        b=54;
+        sum =a + b;
+        printf("%d",sum);
+        return 0;
+            }
